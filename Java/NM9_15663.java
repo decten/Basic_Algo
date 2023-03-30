923d9470d97befcd869127b9fbeaee94e9f9dadd
hyeon <vikim1210@naver.com>
2023-03-30 18:39:49 +0900

    NM9_15663_틀린 걸 알지만 무슨 값을 넣고 비교해야 하는지는 디버깅 해야 알 수 있음

--- Java/NM9_15663.java
+++ Java/NM9_15663.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class NM9_15663 {
     static int N, M;
-    static int[] input_array,selected;
+    static int[] input_array,selected,used;
     static StringBuilder stringBuilder = new StringBuilder();
     static void input(){
         FastReader scanner = new FastReader();
@@ -12,22 +12,28 @@ public class NM9_15663 {
         M = scanner.nextInt();
         input_array = new int[N+1];
         selected = new int[M+1];
+        used = new int[M+1];
         for (int i = 0; i < N; i++) {
             input_array[i] = scanner.nextInt();
         }
         Arrays.sort(input_array);
     }
     static void rec_func(int k){
-        int[] answer = new int[M+1];
         if(k==M){
-            for (int i = 0; i < M; i++) stringBuilder.append(answer[i]).append(' ');
+            for (int i = 0; i < M; i++) stringBuilder.append(selected[i]).append(' ');
             stringBuilder.append('\n');
         }else{
+            int last_candi = 0;
             for (int candi = 0; candi < N; candi++) {
+                if (used[k]==1) continue;
+                if (last_candi == input_array[candi]) continue;
 
-                selected[k] = candi;
+                selected[k] = input_array[candi];
+                last_candi = input_array[candi];
+                used[k] = 1;
                 rec_func(k+1);
                 selected[k] = 0;
+                used[k] = 0;
             }
         }
     }
