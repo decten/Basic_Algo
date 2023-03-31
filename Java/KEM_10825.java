f4c61846db5c7787d594ae9d6e55acc4b92ba090
hyeon <vikim1210@naver.com>
2023-04-01 01:47:57 +0900

    KEM_10825_기본틀 작성

---
+++ Java/KEM_10825.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class KEM_10825 {
+    static void input(){
+        FastReader scan = new FastReader();
+        int N = scan.nextInt();
+        for (int i = 0; i < N; i++) {
+
+        }
+    }
+    public static void main(String args[]){
+        input();
+    }
+    public static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        FastReader(String s) throws IOException{
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+        String next(){
+            while(st==null || !st.hasMoreElements()){
+                try{
+                st = new StringTokenizer(br.readLine());
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+}
