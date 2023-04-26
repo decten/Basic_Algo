54dcb4032edb96958e718613c58371737cf6cb77
hyeon <vikim1210@naver.com>
2023-04-27 01:10:19 +0900

    GuitarLesson_2343_어떻게 이분탐색으로 풀어야 할지 모르겠음

---
+++ Java/GuitarLesson_2343.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class GuitarLesson_2343 {
+    static int N,M;
+    static int[] a;
+    static void input(){
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        M = scan.nextInt();
+        a = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = scan.nextInt();
+        }
+    }
+    static boolean determination(int k){
+        int cnt=1;
+        return cnt>=M;
+    }
+    static void answer(){
+        Arrays.sort(a);
+        long left=0, right=1000000000;
+        while(right>=left){
+
+
+        }
+    }
+    public static void main(String[] args) {
+        input();
+        answer();
+    }
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null||!st.hasMoreElements()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+}
