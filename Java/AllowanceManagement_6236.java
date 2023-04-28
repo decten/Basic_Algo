cfff41e4390fb2b072ba70fa1057ca0bb7bbd727
hyeon <vikim1210@naver.com>
2023-04-28 20:34:39 +0900

    AllowanceManagement_6236_테스트는 통과하는데 실패

--- Java/AllowanceManagement_6236.java
+++ Java/AllowanceManagement_6236.java
@@ -14,20 +14,23 @@ public class AllowanceManagement_6236 {
         }
     }
     static boolean determination(int k){
-        int cnt = 1, remain=k;
+        int cnt = 1, sum=0;
         for (int i = 0; i < N; i++) {
-            if(remain-a[i] < 0){
+            if(sum+a[i] > k){
                 cnt++;
-                remain += k;
+                sum = a[i];
             }else{
-                remain -= a[i];
+                sum += a[i];
             }
         }
         return cnt<=M;
     }
     static void answer(){
         Arrays.sort(a);
-        int left=1, right=10000, ans=0;
+        int left=a[0], right=1000000000, ans=0;
+        for (int i = 0; i < N; i++) {
+            left = Math.max(left,a[i]);
+        }
         while (right>=left){
             int mid = (right+left)/2;
             if(determination(mid)){
@@ -39,7 +42,7 @@ public class AllowanceManagement_6236 {
         }
         System.out.println(ans);
     }
-    static void main(String args[]){
+    static public void main(String args[]){
         input();
         answer();
     }
