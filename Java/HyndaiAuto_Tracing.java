8b1e8d4add196c524b62ae84583aa3617a76f187
hyeon <vikim1210@naver.com>
2023-05-17 01:13:12 +0900

    HyndaiAuto_Tracing_date를 어떻게 갱신해야 하지

--- Java/HyndaiAuto_Tracing.java
+++ Java/HyndaiAuto_Tracing.java
@@ -2,8 +2,48 @@ import java.util.*;
 import java.io.*;
 
 public class HyndaiAuto_Tracing {
-    public static void main(String args[]){
+    static char[][] array;
+    static int N,K;
 
+    static void input(){
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        K = scan.nextInt();
+        array = new char[N][N];
+        for(int i=0; i<N; i++){
+            String row = scan.next();
+            for(int j=0; j<N;j++){
+                array[i][j] = row.charAt(j);
+            }
+        }
+    }
+
+    static void answer(){
+        int ans = 0;
+
+        for(int track=0; track<N; track++){
+            int cnt = 1;
+            int a_cnt = 0;
+            int b_cnt = 0;
+
+            for(int date=0; date<=K;date++){
+                if(cnt==K+1) {
+                    ans = Math.max(ans,Math.abs(a_cnt-b_cnt));
+                    break;
+                }
+                if(array[date][track]=='a') a_cnt++;
+                else b_cnt++;
+                cnt++;
+
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    public static void main(String args[]){
+        input();
+        answer();
     }
     static class FastReader{
         BufferedReader br;
@@ -22,6 +62,9 @@ public class HyndaiAuto_Tracing {
                 }
             }
             return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
         }
     }
 }
