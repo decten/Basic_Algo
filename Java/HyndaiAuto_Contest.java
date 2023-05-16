591d899fdbc3f964694820ac25f61e479aec3a92
hyeon <vikim1210@naver.com>
2023-05-16 23:45:11 +0900

    현대오토에버_다시 풀기

---
+++ Java/HyndaiAuto_Contest.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class HyndaiAuto_Contest {
+    public static void main(String args[]){
+
+    }
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next(){
+            while(st==null||!st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+    }
+}
---
+++ Java/HyndaiAuto_Tracing.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class HyndaiAuto_Tracing {
+    public static void main(String args[]){
+
+    }
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next(){
+            while(st==null||!st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+    }
+}
