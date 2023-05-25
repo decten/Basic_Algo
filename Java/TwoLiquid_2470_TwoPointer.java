d33237d8858b769177823a9726da5f6f570b5d5b
hyeon <vikim1210@naver.com>
2023-05-25 17:07:41 +0900

    TwoLiquid_2470_TwoPointer_left,right를 반복문 변수가 아닌 다른 값을 사용함

--- Java/TwoLiquid_2470_TwoPointer.java
+++ Java/TwoLiquid_2470_TwoPointer.java
@@ -1,21 +1,35 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class TwoLiquid_2470_TwoPointer {
     static int N;
-    static long[] a;
-
+    static int[] a;
+    static int[] b = new int[2];
     static void input(){
         FastReader scan = new FastReader();
         N = scan.nextInt();
-        a = new long[N];
+        a = new int[N];
         for (int i = 0; i < N; i++) {
-            a[i] = scan.nextLong();
+            a[i] = scan.nextInt();
         }
+        Arrays.sort(a);
     }
 
     static void answer(){
-
+        int right = N-1, left=0, ans = Integer.MAX_VALUE, sum = 0;
+        //right=left가 되는 순간 끝이 남
+        while(right>left){
+            sum = a[left]+a[right];
+            if(Math.abs(ans) >= Math.abs(sum)){
+                ans = sum;
+                b[0] = a[left];
+                b[1] = a[right];
+            }
+            if(sum > 0) right--;
+            else left++;
+        }
+        System.out.println(b[0]+" "+b[1]);
     }
 
     public static void main(String[] args) {
