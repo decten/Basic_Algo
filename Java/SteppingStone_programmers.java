155f3cf13deef5b88090b107ede810bd487244bb
hyeon <vikim1210@naver.com>
2023-06-03 00:59:21 +0900

    SteppingStone_programmers_int 배열 생성과 초기화를 동시에 하는 방법

---
+++ Java/SteppingStone_programmers.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class SteppingStone_programmers {
+
+    static int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int ans = solution(25, new int[]{2,14,11,21,17},2);
+        System.out.println(ans);
+    }
+}
