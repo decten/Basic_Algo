55ddc90e34c664aa187ff98c14ef1c0c9b625f24
hyeon <vikim1210@naver.com>
2023-06-17 21:36:35 +0900

    SteppingStone_programmers_왜 이분탐색인지 발상을 생각 못 했음(최소 거리 했을 때->n개 제거 되느냐)

--- Java/SteppingStone_programmers.java
+++ Java/SteppingStone_programmers.java
@@ -1,9 +1,38 @@
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SteppingStone_programmers {
+    static int bin_search(ArrayList<Integer> rocks){
+        int left=0, right=rocks.get(rocks.size()-1), mid = 0;
+        while(right>=left){
+            mid = (right+left)/2;
+            if(deter(mid,rocks)){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return mid;
+    }
+    static boolean deter(int ans, ArrayList<Integer> rocks){
+        int sum = 0;
+        int tmp = rocks.get(0);
+        for (int i = 1; i < rocks.size(); i++) {
+                if(ans>tmp){
+                    sum++;
+                    continue;
+                }
+                tmp = rocks.get(i)-rocks.get(i-1);
+        }
+        return sum>=tmp;
+    }
 
     static int solution(int distance, int[] rocks, int n) {
-        int answer = 0;
+        ArrayList<Integer> arrayList = (ArrayList<Integer>) Arrays.stream(rocks).boxed().collect(
+            Collectors.toList());
+        arrayList.add(distance);
+        Collections.sort(arrayList);
+        int answer = bin_search(arrayList);
         return answer;
     }
 
