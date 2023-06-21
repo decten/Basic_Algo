a43a57a8603cae960dcac62f5bec1d5678ff41f8
hyeon <vikim1210@naver.com>
2023-06-21 20:17:32 +0900

    SharpEye_1637_등차 수열에서 등장 횟수=더 작은 수들의 갯수 & 공차 확인

--- Java/SharpEye_1637.java
+++ Java/SharpEye_1637.java
@@ -15,9 +15,44 @@ public class SharpEye_1637 {
         }
     }
 
+    static boolean deter(int k){
+        long sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += count(a[i][0], a[i][1], a[i][2],k);
+        }
+        return sum % 2 == 1;
+    }
+
+    private static int count(int a, int c, int b, int k) {
+        if(k < a) return 0;
+        if(k > c) return (c-a)/b+1;
+        return (k-a) / b + 1;
+    }
+
+    static void answer(){
+        long left =1 , right = Integer.MAX_VALUE, ans = 0, ansCnt = 0;
+        while(right>=left){
+            long mid = (left+right)/2;
+            if(deter((int)mid)){
+                ans = mid;
+                right = mid -1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        if (ans == 0) {
+            System.out.println("NOTHING");
+        }else{
+            for (int i = 0; i < N; i++) {
+                //마지막은 공차가 맞는지 확인
+                if (a[i][0] <= ans && ans <= a[i][1] && (ans-a[i][0]) % a[i][2] == 0) ansCnt++;
+            }
+        }
+        System.out.println(ans+" "+ansCnt);
+    }
     public static void main(String[] args) {
         input();
-
+        answer();
     }
 
     static class FastReader{
