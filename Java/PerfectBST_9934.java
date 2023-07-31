37f09632f7d67cc0854499e65c21f3d1bf12b716
hyeon <vikim1210@naver.com>
2023-08-01 01:48:31 +0900

    PerfectBST_9934_인오더 트리 복원과 트리 출력

---
+++ Java/PerfectBST_9934.java
@@ -0,0 +1,112 @@
+import java.io.*;
+import java.util.*;
+
+public class PerfectBST_9934 {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){this.data = data;}
+    }
+
+    static int K;
+    static List<Integer> L = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+
+    static void input(){
+        FastReader scan = new FastReader();
+        K = (int) Math.pow(2, scan.nextInt())-1;
+        for (int i = 0; i < K; i++) {
+            L.add(scan.nextInt());
+        }
+    }
+    //중앙 값 노드를 기준으로 왼쪽, 오른쪽 트리 나눠짐
+    static Node buildTreee(int start, int end){
+        //재귀함수 종료
+        if(start>end){
+            return null;
+        }
+
+        int i = (start+end)/2;
+        Node node = new Node(L.get(i));
+
+        if(start==end){
+            return node;
+        }
+
+        node.left = buildTreee(start,i-1);
+        node.right = buildTreee(i+1, end);
+        return node;
+    }
+    static void answer(){
+        Node root = buildTreee(0, L.size()-1);
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                Node node = queue.poll();
+                sb.append(node.data+" ");
+                //전위순회라서 왼쪽 먼저
+                if(node.left!=null) queue.add(node.left);
+                if(node.right!=null) queue.add(node.right);
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        input();
+        answer();
+    }
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
