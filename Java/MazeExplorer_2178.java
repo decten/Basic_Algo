1a035ccdeb5e50d7ab7438685e1b6a511e3d9a07
ppoyami <vikim4911@gmail.com>
2023-08-06 22:28:08 +0900

    MazeExplorer_2178_이전 노드 값을 누적하는 방식으로 경로에 있는 노드들의 거리를 구함

--- Java/MazeExplorer_2178.java
+++ Java/MazeExplorer_2178.java
@@ -1,8 +1,9 @@
 import java.util.*;
 import java.io.*;
 public class MazeExplorer_2178 {
-    static int N,M,ans;
+    static int N,M;
     static int[][] a;
+    static int[][] dist;
     static boolean[][] visit;
     static int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
     static void input(){
@@ -10,6 +11,7 @@ public class MazeExplorer_2178 {
         N = scan.nextInt();
         M = scan.nextInt();
         a = new int[N+1][M+1];
+        dist = new int[N+1][M+1];
         visit = new boolean[N+1][M+1];
 
         for (int i = 1; i <= N; i++) {
@@ -22,13 +24,20 @@ public class MazeExplorer_2178 {
     }
     static void solve(){
         bfs(1,1);
-        System.out.println(ans);
+        System.out.println(dist[N][M]);
     }
     static void bfs(int row, int col){
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M ; j++) {
+                dist[i][j] = -1;
+            }
+        }
+
        Queue<Integer> queue = new LinkedList<>();
        queue.add(row);
        queue.add(col);
        visit[row][col] = true;
+       dist[row][col] = 1;
 
        while(!queue.isEmpty()){
            int x = queue.poll(), y = queue.poll();
@@ -38,12 +47,12 @@ public class MazeExplorer_2178 {
 
                if(nx<0 || ny<0 || nx>N || ny>M) continue;
                if(a[nx][ny] == 0 || visit[nx][ny]) continue;
-               if(nx==N && ny==M) return;
 
                queue.add(nx);
                queue.add(ny);
                visit[nx][ny] = true;
-               ans++;
+               //경로가 없는 노드는 방향에서 제외해야 하는데 그 처리가 안 돼 있음 -> 방문하는 경우에만 값 누적
+               dist[nx][ny] = dist[x][y]+1;
            }
        }
     }
