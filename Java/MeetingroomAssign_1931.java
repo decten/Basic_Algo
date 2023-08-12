57f3f778680df38fd5164af291fb533c93c5142d
hyeon <decten@github.com>
2023-08-13 00:34:49 +0900

    MeetingroomAssign_1931_도착시간이 빠르고 시작 시간도 빠른 것이 우선이 돼야 정답 처리 & 우선순위 큐 사용할 필요 없음 -> 더 복잡해짐 그냥 변수 써도 됨

--- Java/MeetingroomAssign_1931.java
+++ Java/MeetingroomAssign_1931.java
@@ -12,27 +12,39 @@ public class MeetingroomAssign_1931 {
             int x = scan.nextInt(), y = scan.nextInt();
             a.add(new int[] {x,y});
         }
-        //도착 시간이 빠른 것, 늦게 시작하는 것
         Collections.sort(a,(x,y)->
         {
-            return x[1] == y[1] ? y[0]-x[0] :x[1]-y[1];
+            return x[1] == y[1] ? x[0]-y[0] :x[1]-y[1];
         });
     }
     static void solve(){
         PriorityQueue <Integer> meeting = new PriorityQueue<>();
         meeting.add(a.get(0)[1]);
 
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
+        int cnt = 1;
+        for (int i = 1; i < N; i++) {
             int[] rooms = a.get(i);
 
-            if(rooms[0] >= meeting.poll()){
+            if(rooms[0] >= meeting.peek()){
                 cnt++;
-                meeting.add(rooms[i]);
+                meeting.poll();
+                meeting.add(rooms[1]);
             }
 
         }
         System.out.println(cnt);
+
+//        int count = 0;
+//        int prev_end_time = 0;
+//
+//        for(int i = 0; i < N; i++) {
+//            if(prev_end_time <= a.get(i)[0]) {
+//                prev_end_time = a.get(i)[1];
+//                count++;
+//            }
+//        }
+//
+//        System.out.println(count);
     }
     public static void main(String[] args){
         input();
