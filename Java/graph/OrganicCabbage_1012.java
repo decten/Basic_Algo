2875e3fa9d3a948445a3f5f96b18b4cdeada5530
hyun <vikim1210@naver.com>
2023-09-03 17:56:32 +0900

    OrganicCabbage_1012_답은 제대로 나오는데 엔터를 입력해야 다음 테스트케이스 값이 나옴

--- Java/graph/OrganicCabbage_1012.java
+++ Java/graph/OrganicCabbage_1012.java
@@ -8,33 +8,71 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class OrganicCabbage_1012 {
-    static int T,N,M,K;
-    //멀티소스 bfs로 여러 시작점의 큐 시도
-    static Queue<Integer> queue = new LinkedList<>();
+    static FastReader scan = new FastReader();
+    static int N,M,K;
+    static int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    static boolean[][] cabbage;
+    static boolean[][] visit;
     static void input(){
-        FastReader scan = new FastReader();
-        T = scan.nextInt();
-        while (T>0){
-            //M이 먼저 입력 됨
-            M = scan.nextInt();
-            N = scan.nextInt();
-            K = scan.nextInt();
 
-            for (int i = 0; i < K; i++) {
-                //x, y 값을 넣음
-                queue.add(scan.nextInt());
-                queue.add(scan.nextInt());
-            }
-            bfs();
-            T--;
+        M = scan.nextInt();
+        N = scan.nextInt();
+        K = scan.nextInt();
+
+        cabbage = new boolean[N][M];
+        visit = new boolean[N][M];
+
+        for (int i = 0; i < K; i++) {
+            //행 값 먼저 주어짐
+            int col = scan.nextInt();
+            int row = scan.nextInt();
+            //배추 있는 곳으로 체크
+            cabbage[row][col] = true;
         }
-    }
-    public static void bfs(){
 
     }
+    public static void solve(){
+        int cnt = 0;
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if(!visit[i][j]&&cabbage[i][j]) {
+                    cnt++;
+                    bfs(i,j);
+                }
+            }
+        }
+        System.out.println(cnt);
+
+    }
+    public static void bfs(int i, int j){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(i);
+        queue.add(j);
+        visit[i][j] = true;
+
+        while (!queue.isEmpty()){
+            int x = queue.poll(), y = queue.poll();
+            for (int k = 0; k < 4; k++) {
+                int nx = dir[k][0]+x, ny = dir[k][1]+y;
+
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if(!cabbage[nx][ny]) continue;
+                if(visit[nx][ny]) continue;
+
+                queue.add(nx);
+                queue.add(ny);
+                visit[nx][ny] = true;
+            }
+        }
+
+    }
     public static void main(String[] args){
-        input();
+        int T = scan.nextInt();
+        while (T-- > 0){
+            input();
+            solve();
+        }
     }
     static class FastReader{
         BufferedReader br;
