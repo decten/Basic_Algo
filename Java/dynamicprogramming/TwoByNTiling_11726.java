58808ce5824f399c73bbca8bb49afb386e2f00d7
hyun <vikim1210@naver.com>
2023-09-05 01:42:26 +0900

    TwoByNTiling_11726_기초 점화식

---
+++ Java/dynamicprogramming/TwoByNTiling_11726.java
@@ -0,0 +1,58 @@
+package dynamicprogramming;
+
+import java.io.*;
+import java.util.*;
+
+public class TwoByNTiling_11726 {
+
+    static FastReader scan = new FastReader();
+
+    static int N;
+    static int[] Dy;
+
+    static void solve() {
+        Dy[1] = 1;
+        Dy[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            Dy[i] = (Dy[i-2] + Dy[i-1]) % 10007;
+        }
+    }
+
+    static void pro() {
+        N = scan.nextInt();
+        Dy = new int[N + 1];
+        solve();
+
+        System.out.println(Dy[N]);
+    }
+
+    public static void main(String[] args) {
+        pro();
+    }
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
