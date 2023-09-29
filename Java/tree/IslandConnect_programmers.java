9627d3a0f2f16f00206b8e93fe48efe3d4b096a2
hyun <vikim1210@naver.com>
2023-09-30 00:49:52 +0900

    IslandConnect_programmers_연결 여부 확인 -> 유니온 파인드 까먹어버림

--- Java/tree/IslandConnect_programmers.java
+++ Java/tree/IslandConnect_programmers.java
@@ -1,12 +1,23 @@
 package tree;
 
-import java.util.Arrays;
-
 public class IslandConnect_programmers {
 
+    static int[] parent;
     static void solution(int n, int[][] costs) {
         int answer = 0;
+        //악 유니온 파인드 왜 생각 못 했지
+        parent = new int[costs.length];
         System.out.println(answer);
+    }
+
+    static int find(int a){
+        if(parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+    static void union(int x, int y){
+        if(find(x)!=find(y)){
+            parent[y] = x;
+        }
     }
 
     public static void main(String[] args) {
