f8ce63b9d3ea0f278722b6bb9592fb29d888595d
hyun <vikim1210@naver.com>
2023-09-29 17:00:32 +0900

    ConnectedComponent_11724_dfs로 연결 안 될 때마다 개수 추가하면 됨

--- Java/graph/ConnectedComponent_11724.java
+++ Java/graph/ConnectedComponent_11724.java
@@ -6,28 +6,42 @@ import java.util.*;
 public class ConnectedComponent_11724 {
     static FastReader scan = new FastReader();
 
-    static int N,M;
-    static List<Integer>[] a;
+    static int N,M, ans;
+    static List<Integer>[] adj;
+    static boolean[] visited;
     static void input(){
         N = scan.nextInt();
         M = scan.nextInt();
-        a = new List[N+1];
+        adj = new List[N+1];
+        visited = new boolean[N+1];
 
+        for (int i = 1; i <= N; i++) {
+            adj[i] = new ArrayList<>();
+        }
         for (int i = 0; i < M; i++) {
             int from = scan.nextInt(), to = scan.nextInt();
-            a[from].add(to);
+            adj[from].add(to);
         }
 
     }
     static void solve(){
-        BFS();
+        for (int i = 1; i <= N; i++) {
+            if(visited[i]) continue;
+            dfs(i);
+            ans++;
+        }
+        System.out.println(ans);
+    }
+    static void dfs(int x){
+        visited[x] = true;
+        for (int y : adj[x]) {
+            if(visited[y]) continue;
+            dfs(y);
+        }
     }
     public static void main(String[] args){
         input();
         solve();
-    }
-    static void BFS(){
-
     }
     static class FastReader{
         BufferedReader br;
