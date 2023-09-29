39074a1ce9628af7b97ea8e7ef3948d368402fc4
hyun <vikim1210@naver.com>
2023-09-30 00:37:05 +0900

    IslandConnect_programmers_배열 바로 선언하면서 할당하는 방법
    new int[] {}

---
+++ Java/tree/IslandConnect_programmers.java
@@ -0,0 +1,15 @@
+package tree;
+
+import java.util.Arrays;
+
+public class IslandConnect_programmers {
+
+    static void solution(int n, int[][] costs) {
+        int answer = 0;
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) {
+        solution(4, new int[][] {{0,1,1},{0,2,2},{1,2,5},{1,3,1},{2,3,8}});
+    }
+}
