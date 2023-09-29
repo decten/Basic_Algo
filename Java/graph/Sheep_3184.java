6cf2d7bdb3a73e46731183522d7ca061018c6a06
hyun <vikim1210@naver.com>
2023-09-29 23:40:02 +0900

    Sheep_3184_입력을 char로 다시 처리 & 영역 안의 양/늑대 개수 모두 합산

--- Java/graph/Sheep_3184.java
+++ Java/graph/Sheep_3184.java
@@ -6,29 +6,27 @@ public class Sheep_3184 {
     static FastReader scan = new FastReader();
     static int N,M, sheep, wolf, s_cnt, w_cnt;
     static int[][] dir = {{0,1},{1,0},{-1,0},{0,-1}};
-    static String[][] a;
+    static String[] a;
     static boolean[][] visit;
     static void input(){
         N = scan.nextInt();
         M = scan.nextInt();
-        a = new String[N][M];
+        a = new String[N];
         visit = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                a[i][j] = scan.next();
-            }
+                a[i] = scan.next();
         }
-
     }
     static void solve(){
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if(!a[i][j].equals("#")&&!visit[i][j]){
+                if(a[i].charAt(j)!='#'&&!visit[i][j]){
                     s_cnt = 0; w_cnt = 0;
                     dfs(i,j);
-                    if(s_cnt > w_cnt) sheep++;
-                    else wolf++;
+                    if(s_cnt==0&&w_cnt==0) continue;
+                    if(s_cnt > w_cnt) sheep+= s_cnt;
+                    else wolf+=w_cnt;
                 }
             }
         }
@@ -36,8 +34,8 @@ public class Sheep_3184 {
     }
     static void dfs(int i, int j){
         visit[i][j] = true;
-        if(a[i][j].equals("o")) s_cnt++;
-        else if(a[i][j].equals("v")) w_cnt++;
+        if(a[i].charAt(j)=='o') s_cnt++;
+        else if(a[i].charAt(j)=='v') w_cnt++;
 
         for (int k = 0; k < 4; k++) {
             int nx = i + dir[k][0];
@@ -45,7 +43,7 @@ public class Sheep_3184 {
 
             if(nx<0||ny<0||nx>=N||ny>=M) continue;
             if(visit[nx][ny]) continue;
-            if(a[i][j].equals("#")) continue;
+            if(a[i].charAt(j)=='#') continue;
 
             dfs(nx,ny);
         }
