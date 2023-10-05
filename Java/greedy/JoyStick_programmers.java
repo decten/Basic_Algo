aec65e4c71482a4669d600fae5302db1e14e09cc
hyun <vikim1210@naver.com>
2023-10-05 23:50:39 +0900

    JoyStick_programmers_기본 틀

---
+++ Java/greedy/JoyStick_programmers.java
@@ -0,0 +1,12 @@
+package greedy;
+
+public class JoyStick_programmers {
+
+    public int solution(String name) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+
+    }
+}
