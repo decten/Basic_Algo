91ee97d8a00def2a192893a80e8a8553fbf9ce95
hyun <vikim1210@naver.com>
2023-10-11 12:01:55 +0900

    CaesarPassword_programmers_'a'+(alpha-'a'+n)%26

--- Java/etc/CaesarPassword_programmers.java
+++ Java/etc/CaesarPassword_programmers.java
@@ -1,33 +1,27 @@
 package etc;
 
-import java.util.*;
-
 public class CaesarPassword_programmers {
     static void solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
 
         for(int i=0; i<s.length();i++){
             char alpha = s.charAt(i);
-            if(alpha==' '){
-                sb.append(' ');
-                continue;
-            }
-            if(alpha+n > 'Z'){
-                char newAlpha = (char) (alpha+n-'Z');
+            //n이 26 넘어갈 수 있음 -> 나머지 연산
+            if(alpha >= 'a' && alpha <= 'z'){
+                //'a'에 남은 값만 더하는 법을 배움
+                char newAlpha = (char) ((alpha - 'a' + n) % 26 + 'a');
                 sb.append(newAlpha);
                 continue;
             }
-            if(alpha+n > 'z'){
-                char newAlpha = (char) (alpha+n-'z');
+            else if(alpha >= 'A' && alpha <= 'Z'){
+                char newAlpha = (char) ((alpha - 'A' + n) % 26 + 'A');
                 sb.append(newAlpha);
                 continue;
             }
-            char newAlpha = (char) (alpha+n);
-            sb.append(newAlpha);
+            sb.append(alpha);
         }
-        sb.toString();
     }
     public static void main(String args[]){
-        solution("AB",1);
+        solution("a B z",4);
     }
 }
