837a34344629fd340fa3994b7d8a157f06d52afa
hyun <vikim1210@naver.com>
2023-10-14 23:47:24 +0900

    CheckingDistance_programmers_입력 값 처리가 문제가 아니고, 거리 확인 조건을 잘못 둔게 원인이었음

--- Java/graph/CheckingDistance_programmers.java
+++ Java/graph/CheckingDistance_programmers.java
@@ -1,5 +1,7 @@
 package graph;
 
+import java.util.Arrays;
+
 public class CheckingDistance_programmers {
 
     static int[] answer;
@@ -26,10 +28,16 @@ public class CheckingDistance_programmers {
             }
             num++;
         }
+
+        for (int ans : answer) {
+            System.out.println(ans);
+        }
+
     }
     static void dfs(String[] place, int num, int i, int j, int cnt){
         if (cnt > 2) return;
-        if (cnt >=0 && cnt <=2 && place[i].charAt(j) == 'P'){
+        //1, 2회에서 사람이 있는지 확인해야 함
+        if (cnt >0 && cnt <=2 && place[i].charAt(j) == 'P'){
             answer[num] = 0;
             return;
         }
@@ -42,7 +50,7 @@ public class CheckingDistance_programmers {
             if (visited[nx][ny]) continue;
 
             visited[nx][ny] = true;
-            dfs(place,++num,nx,ny,++cnt);
+            dfs(place,num,nx,ny,++cnt);
             visited[nx][ny] = false;
         }
     }
