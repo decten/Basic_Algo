27c583d68d1a42be935b45c9203af650951266a9
hyun <vikim1210@naver.com>
2023-10-23 02:14:20 +0900

    ParkingFee_programmers_총체적 난국
    1. 시간, 요금 변수 따로 둠
    2. out 체크 변수 생성
    3. 요금은 나중에 계산
    4. 인덱스 접근 위해 answer를 int[]로 바꿈

--- Java/etc/ParkingFee_programmers.java
+++ Java/etc/ParkingFee_programmers.java
@@ -4,24 +4,43 @@ import java.util.*;
 public class ParkingFee_programmers {
 
     static int[] solution(int[] fees, String[] records) {
-        int[] answer = {};
+        int[] answer;
         Map<Integer, Integer> carMap = new TreeMap<>();
+        Map<Integer, Integer> feeMap = new TreeMap<>();
+        boolean[] carOut = new boolean[10000];
 
         for (String record : records) {
             int time = Integer.parseInt(record.split(" ")[0].split(":")[0]) * 60 +
                 Integer.parseInt(record.split(" ")[0].split(":")[1]);
-            int number = Integer.parseInt(record.split(" ")[1]);
+            int carNumber = Integer.parseInt(record.split(" ")[1]);
             String flag = record.split(" ")[2];
 
-            if(flag=="OUT"){
-                int fee = (time - carMap.get(number) - fees[0])/fees[3] * fees[4] + fees[1];
-                carMap.put(number,fee);
+            if (flag.equals("OUT")) {
+                feeMap.put(carNumber,
+                    feeMap.getOrDefault(carNumber, 0) + time - carMap.get(carNumber));
+                carOut[carNumber] = true;
             }else{
-                carMap.put(number,time);
+                carMap.put(carNumber, time);
+                carOut[carNumber] = false;
             }
         }
 
-        return carMap.values().stream().mapToInt(Integer::intValue).toArray();
+        int i = 0;
+        answer = new int[feeMap.size()];
+
+        for (int key : feeMap.keySet()) {
+            answer[i] = fees[1];
+            if (!carOut[key]) {
+                feeMap.put(key, feeMap.getOrDefault(key, 0) + (23 * 60 + 59) - carMap.get(key));
+            }
+            if ((int) Math.ceil((feeMap.get(key) - fees[0]) / (float) fees[2]) * fees[3] > 0) {
+                answer[i] = fees[1] + (int) Math.ceil((feeMap.get(key) - fees[0]) / (float) fees[2]) * fees[3];
+            }
+            i++;
+        }
+
+
+        return answer;
     }
 
     public static void main(String[] args) {
