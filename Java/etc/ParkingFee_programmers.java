42bd19aee9659548909417df95a6fb542336da6b
hyun <vikim1210@naver.com>
2023-10-23 02:18:26 +0900

    ParkingFee_programmers_더 큰 범위 변수 혼동 carMap을 사용해야 feeMap이 없는 경우 감당 가능

--- Java/etc/ParkingFee_programmers.java
+++ Java/etc/ParkingFee_programmers.java
@@ -26,9 +26,9 @@ public class ParkingFee_programmers {
         }
 
         int i = 0;
-        answer = new int[feeMap.size()];
+        answer = new int[carMap.size()];
 
-        for (int key : feeMap.keySet()) {
+        for (int key : carMap.keySet()) {
             answer[i] = fees[1];
             if (!carOut[key]) {
                 feeMap.put(key, feeMap.getOrDefault(key, 0) + (23 * 60 + 59) - carMap.get(key));
@@ -44,11 +44,9 @@ public class ParkingFee_programmers {
     }
 
     public static void main(String[] args) {
-        solution(new int[]{180, 5000, 10, 600}, new String[]
+        solution(new int[]{1, 461, 1, 10}, new String[]
             {
-                "05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT",
-                "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN",
-                "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"
+                "00:00 1234 IN"
             }
         );
 
