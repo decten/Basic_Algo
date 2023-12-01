58376e331f9745db6d779c609ad141c67788f36f
HyunA <vikim1210@naver.com>
2023-12-02 01:05:59 +0900

    PartialSum_1182_완료

--- Java/bruteforce/PartialSum_1182.java
+++ Java/bruteforce/PartialSum_1182.java
@@ -1,7 +1,11 @@
 package bruteforce;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class PartialSum_1182 {
     static int N,S, answer;
