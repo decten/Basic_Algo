209bc9820487931a612e78a814cf4ddcb62bc8bf
HyunA <vikim1210@naver.com>
2023-12-04 22:20:57 +0900

    SharpEye_1637_완료

--- Java/binarysearch/SharpEye_1637.java
+++ Java/binarysearch/SharpEye_1637.java
@@ -4,9 +4,11 @@ import java.util.*;
 import java.io.*;
 
 public class SharpEye_1637 {
+
     static int N;
     static int[][] a;
-    static void input(){
+
+    static void input() {
         FastReader scan = new FastReader();
         N = scan.nextInt();
         a = new int[N][3];
@@ -17,74 +19,84 @@ public class SharpEye_1637 {
         }
     }
 
-    static boolean deter(int k){
+    static boolean deter(int k) {
         long sum = 0;
         for (int i = 0; i < N; i++) {
-            sum += count(a[i][0], a[i][1], a[i][2],k);
+            sum += count(a[i][0], a[i][1], a[i][2], k);
         }
         return sum % 2 == 1;
     }
 
     private static int count(int a, int c, int b, int k) {
-        if(k < a) return 0;
-        if(k > c) return (c-a)/b+1;
-        return (k-a) / b + 1;
+        if (k < a) {
+            return 0;
+        }
+        if (k > c) {
+            return (c - a) / b + 1;
+        }
+        return (k - a) / b + 1;
     }
 
-    static void answer(){
-        long left =1 , right = Integer.MAX_VALUE, ans = 0, ansCnt = 0;
-        while(right>=left){
-            long mid = (left+right)/2;
-            if(deter((int)mid)){
+    static void answer() {
+        long left = 1, right = Integer.MAX_VALUE, ans = 0, ansCnt = 0;
+        while (right >= left) {
+            long mid = (left + right) / 2;
+            if (deter((int) mid)) {
                 ans = mid;
-                right = mid -1;
-            }else{
+                right = mid - 1;
+            } else {
                 left = mid + 1;
             }
         }
         if (ans == 0) {
             System.out.println("NOTHING");
-        }else{
+        } else {
             for (int i = 0; i < N; i++) {
                 //마지막은 공차가 맞는지 확인
-                if (a[i][0] <= ans && ans <= a[i][1] && (ans-a[i][0]) % a[i][2] == 0) ansCnt++;
+                if (a[i][0] <= ans && ans <= a[i][1] && (ans - a[i][0]) % a[i][2] == 0) {
+                    ansCnt++;
+                }
             }
         }
-        System.out.println(ans+" "+ansCnt);
+        System.out.println(ans + " " + ansCnt);
     }
+
     public static void main(String[] args) {
         input();
         answer();
     }
 
-    static class FastReader{
+    static class FastReader {
+
         BufferedReader br;
         StringTokenizer st;
 
-        FastReader(){
+        FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        String next(){
-            while(st==null||!st.hasMoreElements()){
-                try{
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
                     st = new StringTokenizer(br.readLine());
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
-        String nextLine(){
-           String str = "";
-           try{
-               str = br.readLine();
-           }catch (IOException e){
-               e.printStackTrace();
-           }
-           return str;
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
         }
-        int nextInt(){
+
+        int nextInt() {
             return Integer.parseInt(next());
         }
     }
