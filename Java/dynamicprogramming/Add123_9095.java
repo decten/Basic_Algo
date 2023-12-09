6d5884955789da7eeaef946a706dfd338f6521f1
HyunA <vikim1210@naver.com>
2023-12-09 22:17:09 +0900

    Add123_9095_완료

--- Java/dynamicprogramming/Add123_9095.java
+++ Java/dynamicprogramming/Add123_9095.java
@@ -4,12 +4,14 @@ import java.io.*;
 import java.util.*;
 
 public class Add123_9095 {
+
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
     //n은 11보다 작음
     static int[] Dy = new int[15];
-    static void pro(){
+
+    static void pro() {
         int T = scan.nextInt();
         for (int i = 0; i < T; i++) {
             int N = scan.nextInt();
@@ -17,41 +19,44 @@ public class Add123_9095 {
         }
         System.out.println(sb.toString());
     }
-    static void solve(){
+
+    static void solve() {
         Dy[1] = 1;
         Dy[2] = 2;
         Dy[3] = 4;
 
         for (int i = 4; i <= 11; i++) {
-            Dy[i] = Dy[i-1]+Dy[i-2]+Dy[i-3];
+            Dy[i] = Dy[i - 1] + Dy[i - 2] + Dy[i - 3];
         }
 
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         solve();
         pro();
     }
-    static class FastReader{
+
+    static class FastReader {
+
         BufferedReader br;
         StringTokenizer st;
 
-        FastReader(){
+        FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        String next(){
-            while (st==null||!st.hasMoreElements()){
-                try{
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
                     st = new StringTokenizer(br.readLine());
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
 
-        int nextInt(){
+        int nextInt() {
             return Integer.parseInt(next());
         }
     }
