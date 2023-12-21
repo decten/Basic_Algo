26ad491fc534e0b43d47dd16c8123c0462594e0c
hyeon <decten@github.com>
2023-12-21 22:21:05 +0900

    PerfectBST_9934_완료

--- Java/graph/PerfectBST_9934.java
+++ Java/graph/PerfectBST_9934.java
@@ -4,57 +4,61 @@ import java.io.*;
 import java.util.*;
 
 public class PerfectBST_9934 {
-    static class Node{
+    static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data){this.data = data;}
+        Node(int data) {
+            this.data = data;
+        }
     }
 
     static int K;
     static List<Integer> L = new ArrayList<>();
     static StringBuilder sb = new StringBuilder();
 
-    static void input(){
+    static void input() {
         FastReader scan = new FastReader();
-        K = (int) Math.pow(2, scan.nextInt())-1;
+        K = (int) Math.pow(2, scan.nextInt()) - 1;
         for (int i = 0; i < K; i++) {
             L.add(scan.nextInt());
         }
     }
+
     //중앙 값 노드를 기준으로 왼쪽, 오른쪽 트리 나눠짐
-    static Node buildTreee(int start, int end){
+    static Node buildTreee(int start, int end) {
         //재귀함수 종료
-        if(start>end){
+        if (start > end) {
             return null;
         }
 
-        int i = (start+end)/2;
+        int i = (start + end) / 2;
         Node node = new Node(L.get(i));
 
-        if(start==end){
+        if (start == end) {
             return node;
         }
 
-        node.left = buildTreee(start,i-1);
-        node.right = buildTreee(i+1, end);
+        node.left = buildTreee(start, i - 1);
+        node.right = buildTreee(i + 1, end);
         return node;
     }
-    static void answer(){
-        Node root = buildTreee(0, L.size()-1);
+
+    static void answer() {
+        Node root = buildTreee(0, L.size() - 1);
 
         Queue<Node> queue = new LinkedList<>();
         queue.add(root);
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int n = queue.size();
             for (int i = 0; i < n; i++) {
                 Node node = queue.poll();
-                sb.append(node.data+" ");
+                sb.append(node.data + " ");
                 //전위순회라서 왼쪽 먼저
-                if(node.left!=null) queue.add(node.left);
-                if(node.right!=null) queue.add(node.right);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
             }
             sb.append('\n');
         }
@@ -66,6 +70,7 @@ public class PerfectBST_9934 {
         input();
         answer();
     }
+
     static class FastReader {
         BufferedReader br;
         StringTokenizer st;
