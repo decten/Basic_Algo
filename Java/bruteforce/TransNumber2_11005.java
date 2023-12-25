c3fbc66c6fbd12833d57bcd8f89418f88ae45594
hyeon <decten@github.com>
2023-12-26 01:43:55 +0900

    TransNumber2_11005_완성

--- Java/bruteforce/TransNumber2_11005.java
+++ Java/bruteforce/TransNumber2_11005.java
@@ -5,40 +5,60 @@ import java.io.*;
 
 public class TransNumber2_11005 {
     static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
     static int N, B;
-    static void input(){
+
+    static void input() {
         N = scan.nextInt();
         B = scan.nextInt();
     }
 
-    static void solve(){
+    static void solve() {
+        int k = 0;
+        while (Math.pow(B, k) <= N) {
+            k++;
+        }
+        if (Math.pow(B, k) > N) k--;
 
+        for (; k >= 0; k--) {
+            if (Math.pow(B, k) > N) {
+                sb.append(0);
+                continue;
+            }
+            int mod = (int) (N / Math.pow(B, k));
+            if (mod > 9) sb.append((char) ((mod - 10) + 'A'));
+            else sb.append(mod);
+            N = N % (int) Math.pow(B, k);
+        }
+
+        System.out.println(sb.toString());
     }
 
     public static void main(String[] args) {
         input();
         solve();
     }
-    static class FastReader{
+
+    static class FastReader {
         StringTokenizer st;
         BufferedReader br;
 
-        FastReader(){
+        FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        String next(){
-            while (st==null||!st.hasMoreElements()){
-                try{
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
                     st = new StringTokenizer(br.readLine());
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
 
-        Integer nextInt(){
+        Integer nextInt() {
             return Integer.parseInt(next());
         }
     }
