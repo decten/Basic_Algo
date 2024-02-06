d9038f54522ade4ce0addb1203efa8aa6fcc97c3
HyunA <vikim1210@naver.com>
2024-02-06 22:16:58 +0900

    NM9_15663_완료

--- Java/bruteforce/NM9_15663.java
+++ Java/bruteforce/NM9_15663.java
@@ -1,83 +1,86 @@
 package bruteforce;
 
-import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class NM9_15663 {
+
     static int N, M;
-    static int[] input_array,selected,used;
+    static int[] input_array, selected, used;
     static StringBuilder stringBuilder = new StringBuilder();
-    static void input(){
+
+    static void input() {
         FastReader scanner = new FastReader();
         N = scanner.nextInt();
         M = scanner.nextInt();
         input_array = new int[N];
-        selected = new int[M+1];
-        used = new int[N+1];
+        selected = new int[M + 1];
+        used = new int[N + 1];
         for (int i = 0; i < N; i++) {
             input_array[i] = scanner.nextInt();
         }
         Arrays.sort(input_array);
     }
-    static void rec_func(int k){
-        if(k==M){
-            for (int i = 0; i < M; i++) stringBuilder.append(selected[i]).append(' ');
+
+    static void rec_func(int k) {
+        if (k == M) {
+            for (int i = 0; i < M; i++) {
+                stringBuilder.append(selected[i]).append(' ');
+            }
             stringBuilder.append('\n');
-        }else{
+        } else {
             int last_candi = 0;
             for (int candi = 0; candi < N; candi++) {
-                if (used[candi]==1) continue;
-                if (last_candi == input_array[candi]) continue;
+                if (used[candi] == 1) {
+                    continue;
+                }
+                if (last_candi == input_array[candi]) {
+                    continue;
+                }
 
                 selected[k] = input_array[candi];
                 last_candi = input_array[candi];
                 used[candi] = 1;
-                rec_func(k+1);
+                rec_func(k + 1);
                 selected[k] = 0;
                 used[candi] = 0;
             }
         }
     }
 
-    static public void main(String[] args){
+    static public void main(String[] args) {
         input();
         rec_func(0);
 
         System.out.print(stringBuilder.toString());
 
     }
-    static public class FastReader{
+
+    static public class FastReader {
+
         BufferedReader br;
         StringTokenizer st;
 
-        FastReader(){
+        FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
-        FastReader(String s) throws IOException{
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
 
-        String next(){
-            while(st==null||!st.hasMoreElements()){
-                try{
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
                     st = new StringTokenizer(br.readLine());
-                }catch (IOException e){
+                } catch (IOException e) {
+                    //noinspection CallToPrintStackTrace
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
-        String nextLine(){
-            String str = "";
-            try {
-                str = br.readLine();
-            }catch (IOException e){
-                e.printStackTrace();
-            }
-            return str;
-        }
-        int nextInt(){
+
+        int nextInt() {
             return Integer.parseInt(next());
         }
     }
