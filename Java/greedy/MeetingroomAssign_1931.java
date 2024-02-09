74df4e0117c7877fbe40987a7712f19aebbfac01
hyeon <decten@github.com>
2024-02-09 21:57:45 +0900

    MeetingroomAssign_1931_완료

--- Java/greedy/MeetingroomAssign_1931.java
+++ Java/greedy/MeetingroomAssign_1931.java
@@ -7,27 +7,29 @@ public class MeetingroomAssign_1931 {
     static int N;
     //배열 형태의 List
     static List<int[]> a = new ArrayList<>();
-    static void input(){
+
+    static void input() {
         FastReader scan = new FastReader();
         N = scan.nextInt();
         for (int i = 0; i < N; i++) {
             int x = scan.nextInt(), y = scan.nextInt();
-            a.add(new int[] {x,y});
+            a.add(new int[]{x, y});
         }
-        Collections.sort(a,(x,y)->
+        Collections.sort(a, (x, y) ->
         {
-            return x[1] == y[1] ? x[0]-y[0] :x[1]-y[1];
+            return x[1] == y[1] ? x[0] - y[0] : x[1] - y[1];
         });
     }
-    static void solve(){
-        PriorityQueue <Integer> meeting = new PriorityQueue<>();
+
+    static void solve() {
+        PriorityQueue<Integer> meeting = new PriorityQueue<>();
         meeting.add(a.get(0)[1]);
 
         int cnt = 1;
         for (int i = 1; i < N; i++) {
             int[] rooms = a.get(i);
 
-            if(rooms[0] >= meeting.peek()){
+            if (rooms[0] >= meeting.peek()) {
                 cnt++;
                 meeting.poll();
                 meeting.add(rooms[1]);
@@ -48,28 +50,32 @@ public class MeetingroomAssign_1931 {
 //
 //        System.out.println(count);
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         input();
         solve();
     }
-    static class FastReader{
+
+    static class FastReader {
         StringTokenizer st;
         BufferedReader br;
 
-        FastReader(){
+        FastReader() {
             br = new BufferedReader(new InputStreamReader(System.in));
         }
-        String next(){
-            while(st==null||!st.hasMoreElements()){
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
-        int nextInt(){
+
+        int nextInt() {
             return Integer.parseInt(next());
         }
     }
