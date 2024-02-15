0491d30698118602ce301af1b50f2048e12728bf
HyunA <vikim1210@naver.com>
2024-02-16 03:57:33 +0900

    LadderManipulation_15684_초안 & array 삭제

--- Java/bruteforce/LadderManipulation_15684.java
+++ Java/bruteforce/LadderManipulation_15684.java
@@ -10,7 +10,6 @@ import java.util.StringTokenizer;
 public class LadderManipulation_15684 {
 
     static FastReader scan = new FastReader();
-    static List<ArrayList<Integer>> array = new ArrayList<>();
     static boolean[][] visited;
     static int N,M,H, ans;
 
@@ -18,32 +17,68 @@ public class LadderManipulation_15684 {
         N = scan.nextInt();
         M = scan.nextInt();
         H = scan.nextInt();
+        visited = new boolean[N + 1][H + 1];
+        ans = -1;
 
-        visited = new boolean[N+1][M+1];
-
-        for (int i = 1; i <= N; i++) {
-            array.add(i, new ArrayList<>());
-        }
 
         for (int i = 0; i < M; i++) {
             int row = scan.nextInt();
             int line = scan.nextInt();
 
-            array.get(row).add(line);
+            visited[line][row] = true;
         }
     }
 
     static void solve() {
         int cnt = 0;
-        dfs(cnt);
 
-    }
-
-    static void dfs(int cnt){
-        if(cnt==3){
+        if (M == 0 || check()) {
+            ans = 0;
             return;
         }
 
+        dfs(1, 1, cnt);
+
+    }
+
+    private static boolean check() {
+        for (int i = 1; i <= N; i++) {
+            int current = i;
+            for (int j = 1; j <= H; j++) {
+                if (visited[i - 1][j]) {
+                    i--;
+                } else if (visited[i][j]) {
+                    i++;
+                }
+            }
+            if (current != i) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void dfs(int i, int j, int cnt) {
+        System.out.println(i + " " + j + " " + cnt);
+        if (i > N || j > H || cnt == 3) {
+            return;
+        }
+
+        if (visited[i][j] ||
+            visited[i - 1][j] ||
+            visited[i + 1][j]) {
+            dfs(i + 1, j + 1, cnt);
+            dfs(i, j + 1, cnt);
+        }
+
+        if (check()) {
+            ans = cnt;
+            return;
+        }
+
+        visited[i][j + 1] = true;
+        dfs(i, j + 1, cnt + 1);
+        visited[i][j + 1] = false;
 
     }
 
